14e9da1357adbdf6bcc642ecb6902a4f2c8184a0
Anant Narayanan <anant@kix.in>
2013-02-07 14:59:22 -0800

    Add exercise3

---
+++ exercise3.js
@@ -0,0 +1,11 @@
+
+window.firebaseRef = null;
+$(function() {
+  var key = "firebase-codecademy";
+  if (!localStorage.getItem(key)) {
+    var hash = Math.random().toString(36).slice(2);
+    localStorage.setItem("firebase-codecademy", hash);
+  }
+  var url = "https://" + localStorage.getItem(key) + ".firebaseio-demo.com";
+  $("#result").html("<a target='_blank' href='" + url + "'>Click to see your Firebase!</a>");
+});
