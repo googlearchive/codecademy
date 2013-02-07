c64190369be9545b2866de51f80d59065a80a6f5
Anant Narayanan <anant@kix.in>
2013-02-07 14:27:30 -0800

    Initial codecademy lesson import

---
+++ exercise1.js
@@ -0,0 +1,14 @@
+
+window.firebaseRef = null;
+$(function() {
+  var key = "firebase-codecademy";
+  if (!localStorage.getItem(key)) {
+    var hash = Math.random().toString(36).slice(2);
+    localStorage.setItem("firebase-codecademy", hash);
+  }
+  var url = "https://" + localStorage.getItem(key) + ".firebaseio-demo.com/";
+  window.firebaseRef = new Firebase(url);
+  firebaseRef.child("exercise1").on("value", function(snap) {
+    $("#result").innerHTML("You wrote " + JSON.stringify(snap.val()) + " into exercise1!");
+  });
+});
