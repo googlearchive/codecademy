be46dfd2a05551e5c23e02973a45886f22adb6db
Anant Narayanan <anant@kix.in>
2013-02-07 16:02:21 -0800

    Add exercise 2 for ordered chapter

---
+++ ordered/exercise2.js
@@ -0,0 +1,11 @@
+
+window.firebaseRef = null;
+$(function() {
+  var key = "firebase-codecademy";
+  if (!localStorage.getItem(key)) {
+    var hash = Math.random().toString(36).slice(2);
+    localStorage.setItem("firebase-codecademy", hash);
+  }
+  var url = "https://" + localStorage.getItem(key) + ".firebaseio-demo.com";
+  window.firebaseRef = new Firebase(url).child("ordered/todo");
+});
